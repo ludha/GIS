2f0c2287300a4c525bec4023af5572a313235508
ludha <ludhapemalang@gmail.com~>
2023-09-29 12:50:59 +0700

    LUDHA

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    const pointTable = document.getElementById("gisTable").getElementsByTagName('tbody')[0];
+    const pointTable = document.getElementById("gisTable2").getElementsByTagName('tbody')[0];
 
     fetch("polygon.json") // Ganti "data.json" dengan nama file JSON Anda
         .then(response => response.json())
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    const pointTable = document.getElementById("gisTable").getElementsByTagName('tbody')[0];
+    const pointTable = document.getElementById("gisTable3").getElementsByTagName('tbody')[0];
 
     fetch("polygon.json") // Ganti "data.json" dengan nama file JSON Anda
         .then(response => response.json())
